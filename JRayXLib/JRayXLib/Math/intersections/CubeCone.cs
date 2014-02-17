e5849fdb87d3f0e7f6b542194a271e3846a75e34
Immanuel Hayden <immanuel.hayden@gmail.com>
2014-02-17 21:15:22 +0100

    remove interim index accessor

--- JRayXLib/JRayXLib/Math/intersections/CubeCone.cs
+++ JRayXLib/JRayXLib/Math/intersections/CubeCone.cs
@@ -18,66 +18,52 @@ namespace JRayXLib.Math.intersections
 	 * @return
 	 */
         public static bool IsCubeEnclosingCone(Vect3 cubeCenter, double cubeWidthHalf, Vect3 conePosition, Vect3 coneAxis, double coneAxisLength, double coneCosPhi){
-            Vect3 planeNormal = new Vect3(0),
-                  planePoint = new Vect3(cubeCenter);
-		
-            if(!PointCube.Encloses(cubeCenter, cubeWidthHalf, conePosition))
-                return false;
-		
-            for(int a=0;a<3;a++){
-                planeNormal[a] = 1;
-			
-                for(int d=0;d<2;d++){
-                    planePoint[a] = planePoint[a] - planeNormal[a] * cubeWidthHalf;
-					
-                    if(PlaneCone.IsPlaneIntersectingCone(planeNormal, planePoint, conePosition, coneAxis, coneAxisLength, coneCosPhi))
-                        return false;
-
-                    planePoint[a] = cubeCenter[a];
-                    planeNormal[a] *= -1;
-                }
-
-                planeNormal[a] = 0;
-            }
-		
-            return true;
+            
+            return PointCube.Encloses(cubeCenter, cubeWidthHalf, conePosition) &&
+                   !IsCubeIntersectingCone(cubeCenter, cubeWidthHalf, conePosition, coneAxis, coneAxisLength, coneCosPhi);
         }
 	
         /**
-	 * Returns true if, and only if, the cube is intersecting the cone. This is true if a random point of the cone is inside the cube or
-	 * the cone is intersecting any of the cube's border areas.
-	 * 
-	 * @param cubeCenter
-	 * @param cubeWidthHalf
-	 * @param conePosition
-	 * @param coneAxis
-	 * @param coneAxisLength
-	 * @param coneCosPhi
-	 * @return
-	 */
-        public static bool IsCubeIntersectingCone(Vect3 cubeCenter, double cubeWidthHalf, Vect3 conePosition, Vect3 coneAxis, double coneAxisLength, double coneCosPhi){
-            Vect3 planeNormal = new Vect3(0),
-                  planePoint = new Vect3(cubeCenter);
-		
-            if(PointCube.Encloses(cubeCenter, cubeWidthHalf, conePosition))
+	     * Returns true if, and only if, the cube is intersecting the cone. This is true if a random point of the cone is inside the cube or
+	     * the cone is intersecting any of the cube's border areas.
+	     * 
+	     * @param cubeCenter
+	     * @param cubeWidthHalf
+	     * @param conePosition
+	     * @param coneAxis
+	     * @param coneAxisLength
+	     * @param coneCosPhi
+	     * @return
+	     */
+        public static bool IsCubeIntersectingCone(Vect3 cubeCenter, double cubeWidthHalf, Vect3 conePosition, Vect3 coneAxis, double coneAxisLength, double coneCosPhi)
+        {
+            // --- X ---
+            var planePoint = new Vect3(cubeCenter.X - cubeWidthHalf, cubeCenter.Y, cubeCenter.Z);
+            var planeNormal = new Vect3(cubeWidthHalf, 0, 0);
+		    if(AreaCone.IsAreaIntersectingCone(planeNormal, planePoint, cubeWidthHalf, conePosition, coneAxis, coneAxisLength, coneCosPhi))
                 return true;
-		
-            for(int a=0;a<3;a++){
-                planeNormal[a] = 1;
-			
-                for(int d=0;d<2;d++){
-                    planePoint[a] = planePoint[a] - planeNormal[a] * cubeWidthHalf;
-					
-                    if(AreaCone.IsAreaIntersectingCone(planeNormal, planePoint, cubeWidthHalf, conePosition, coneAxis, coneAxisLength, coneCosPhi))
-                        return true;
+            planePoint.X += 2*cubeWidthHalf;
+            if (AreaCone.IsAreaIntersectingCone(planeNormal, planePoint, cubeWidthHalf, conePosition, coneAxis, coneAxisLength, coneCosPhi))
+                return true;
 
-                    planePoint[a] = cubeCenter[a];
-                    planeNormal[a] *= -1;
-                }
+            // --- Y ---
+            planePoint = new Vect3(cubeCenter.X, cubeCenter.Y - cubeWidthHalf, cubeCenter.Z);
+            planeNormal = new Vect3(0, cubeWidthHalf, 0);
+            if (AreaCone.IsAreaIntersectingCone(planeNormal, planePoint, cubeWidthHalf, conePosition, coneAxis, coneAxisLength, coneCosPhi))
+                return true;
+            planePoint.Y += 2 * cubeWidthHalf;
+            if (AreaCone.IsAreaIntersectingCone(planeNormal, planePoint, cubeWidthHalf, conePosition, coneAxis, coneAxisLength, coneCosPhi))
+                return true;
 
-                planeNormal[a] = 0;
-            }
-            //System.out.println("ni with "+cubeCenter+" +/-"+cubeWidthHalf);
+            // --- Z ---
+            planePoint = new Vect3(cubeCenter.X, cubeCenter.Y, cubeCenter.Z - cubeWidthHalf);
+            planeNormal = new Vect3(0, 0, cubeWidthHalf);
+            if (AreaCone.IsAreaIntersectingCone(planeNormal, planePoint, cubeWidthHalf, conePosition, coneAxis, coneAxisLength, coneCosPhi))
+                return true;
+            planePoint.Z += 2 * cubeWidthHalf;
+            if (AreaCone.IsAreaIntersectingCone(planeNormal, planePoint, cubeWidthHalf, conePosition, coneAxis, coneAxisLength, coneCosPhi))
+                return true;
+
             return false;
         }
     }
