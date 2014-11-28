2bcfd588ca6e8f47cef78ef426c73ab3d56e863f
Immanuel Hayden <immanuel.hayden@gmail.com>
2014-11-28 20:21:39 +0100

    add octree-tests

---
+++ JRayXLib/JRayXLibTests/OctreeTests.cs
@@ -0,0 +1,47 @@
+﻿using JRayXLib.Colors;
+using JRayXLib.Shapes;
+using JRayXLib.Struct;
+using NUnit.Framework;
+
+namespace JRayXLibTests
+{
+    [TestFixture]
+    class OctreeTests
+    {
+        [Test]
+        public void OctreeFromOutside()
+        {
+            var ray = new Ray
+                {
+                    Origin = new Vect3 {X = -5},
+                    Direction = new Vect3 {X = 1}
+                };
+
+            var octree = new Octree2(new Vect3(), 2);
+            var sphere = new Sphere(new Vect3(), 2, Color.Red);
+            octree.Insert(sphere);
+
+            var collisionDetails = octree.GetFirstCollision(ray);
+            Assert.That(!double.IsInfinity(collisionDetails.Distance));
+            Assert.That(collisionDetails.Obj, Is.EqualTo(sphere));
+        }
+
+        [Test]
+        public void OctreeFromInside()
+        {
+            var ray = new Ray
+            {
+                Origin = new Vect3 { X = -5 },
+                Direction = new Vect3 { X = 1 }
+            };
+
+            var octree = new Octree2(new Vect3(), 10);
+            var sphere = new Sphere(new Vect3(), 2, Color.Red);
+            octree.Insert(sphere);
+
+            var collisionDetails = octree.GetFirstCollision(ray);
+            Assert.That(!double.IsInfinity(collisionDetails.Distance));
+            Assert.That(collisionDetails.Obj, Is.EqualTo(sphere));
+        }
+    }
+}
